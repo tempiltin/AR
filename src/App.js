0da08e1c056d37f0f99d3429c0168f5a312fc81d
tempiltin <temurbekshukurov0707@gmail.com>
2022-09-27 12:26:21 +0500

    asd

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import "./App.css"
+const App = () => {
+  return (
+    <header>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-12">
+            <div className="bar mt-2"></div>
+          </div>
+          <div className="col-2">
+            <div className="card mt-2"> {/*  */}</div>
+          </div>
+          <div className="col-10">
+          <div className="container-fluid">
+            <div className="row mt-2">
+              <div className="col-9">
+
+              </div>
+              <div className="col-3">
+                <div className="card"></div>
+              </div>
+            </div>
+          </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default App
